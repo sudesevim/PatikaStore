19c2e60b6d9b7c22fc4878c4eefa18863e9cc6db
Sude Sevim <sudesevim0@gmail.com>
2024-08-13 17:29:18 +0300

    tb eklendi

---
+++ tb.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from 'react-native';
+
+function tb () {
+    return (
+        <SafeAreaView>
+            <Text>Tatilbudur</Text>
+        </SafeAreaView>
+    );
+}
+
+export default tb;
